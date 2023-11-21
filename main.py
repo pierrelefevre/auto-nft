eef9b3ba9dfd338feff6fc90eeb8ce0143b8ef50
Pierre Le Fevre <hello@pierrelf.com>
2023-11-21 14:22:41 +0100

    Add files

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 import generate
 
-scenario = "Bazooki is a cute bazooka mascot. Write 10 scenarios in which Bazooki might find himself. Write one per line."
-
+scenario = "Bazooki is a cute bazooka mascot. Write 12 scenarios in which Bazooki might find himself. Write one per line."
 scenarios = generate.text(scenario).split("\n")
 
-print("Scenarios:" + str(len(scenarios)))
+print(f"Generated {str(len(scenarios))} scenarios")
 
-# for testing, only use the first scenario
-scenarios = scenarios[:1]
+# Skip first and last scenario as they are usually some kind of intro or outro
+scenarios = scenarios[1:-1]
+
+items = []
 
 for i, s in enumerate(scenarios):
+    id = i+1
 
+    print(f"Generating NFT, {id} of {len(scenarios)}")
+
+    # Generate image
     prompt = generate.text(
         "Write a short but descriptive prompt for image generation about this scenario. The prompt should be a few space separated words. Make sure to emphasize that the character is a bazooka.\n" + s)
-    path = generate.image(i, "cute bazooka mascot " + prompt)
-    description = generate.text(
-        "Write a short third person summary for this scenario.\n" + s + "\nReturn only the summary.")
+    generate.image(id, "cute bazooka mascot ultra realistic 4k" + prompt)
+
+    # Generate description
     title = generate.text(
-        "What is a good title for this scenario?\n" + s + "\nReturn only the title.")
+        "What is a good title for this scenario? It is about Bazooki the cute bazooka mascot\n" + s + "\nReturn only the title.").replace(",", "").replace("\"", "")
+    description = generate.text(
+        "Write a short third person summary for this scenario.  It is about Bazooki the cute bazooka mascot\n" + s + "\nReturn only the summary.").replace(",", "").replace("\"", "")
 
-    print("Title: " + title)
-    print("Description: " + description)
-    print("Path: " + path)
-    print("Prompt: " + prompt)
+    # Save item
+    new_item = {
+        "tokenID": id,
+        "name": title,
+        "description": description,
+        "file_name": f"{id}.png",
+    }
 
-# generate.image(0, "cat riding a skateboard")
-# print(generate.text("write the lore behind larry the cat riding a skateboard"))
+    items.append(new_item)
+
+# Write metadata file
+print("Writing metadata file")
+with open("metadata.csv", "w+") as file:
+    file.write("tokenID,name,description,file_name\n")
+    for item in items:
+        file.write(str(item["tokenID"]) + "," + item["name"] + "," +
+                   item["description"] + "," + item["file_name"] + "\n")
+
+print("Done")
---
+++ steps.md
@@ -0,0 +1,32 @@
+## Generate (live coding)
+Based on main.py
+
+## Upload to opensea
+
+1. Go to Studio
+2. Create new project
+3. Drop a collection
+4. Upload logo 
+5. Fill in name and symbol
+6. Choose Polygon
+7. Deploy!
+8. View transaction on polygonscan, explain a bit about what's going on
+9. Go to editor
+10. Set theme to covered
+11. Save collection
+12. Go to items
+13. Upload media
+14. Upload metadata
+15. Click upload
+16. Click continue
+17. Go to settings
+18. Set number of items to 12 or whatever is generated in main.py
+19. Set min start time to now
+20. Set mint public stage to end in 2 minutes
+21. Publish drop
+22. Buy one
+23. Show on opensea
+24. Go back to studio
+25. Select collection
+26. Go to items
+27. Click reveal
